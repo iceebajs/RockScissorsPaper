9b414902c906f80ab0c0151395b53dc69236e23e
ramonyboi <dota2forfreebro@gmail.com>
2020-06-07 16:57:19 +0300

    Add project files.

---
+++ RockScissorsPaper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockScissorsPaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string s in args)
+                Console.WriteLine(s);
+            Console.Read();
+        }
+    }
+
+}
